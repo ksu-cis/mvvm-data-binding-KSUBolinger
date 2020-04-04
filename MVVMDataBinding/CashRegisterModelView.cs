0f693037fdd24f0c3ce7ec51dc1472c1e892cd75
Easton Bolinger <bolinger@ksu.edu>
2020-04-03 23:56:29 -0500

    Partially Done

---
+++ MVVMDataBinding/CashRegisterModelView.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using CashRegister;
+
+namespace MVVMDataBinding
+{
+    public class CashRegisterModelView : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Notifies of property changed
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The CashDrawer this class uses
+        /// </summary>
+        CashDrawer drawer = new CashDrawer();
+
+        /// <summary>
+        /// thet total current value of the drawer
+        /// </summary>
+        public double TotalValue => drawer.TotalValue;
+
+        public int Pennies
+        {
+            get => drawer.Pennies;
+
+            set
+            {
+                if (drawer.Pennies == value || value < 0) return;
+                var quantity = value - drawer.Pennies;
+                if(quantity > 0)
+                {
+                    drawer.AddCoin(Coins.Penny, quantity);
+                }
+                else
+                {
+                    drawer.RemoveCoin(Coins.Penny, -quantity);
+                }
+                InvokePropertyChanged("Pennies");
+            }
+        }
+
+        public int Nickles
+        {
+            get => drawer.Nickles;
+
+            set
+            {
+                if (drawer.Nickles == value || value < 0) return;
+                var quantity = value - drawer.Nickles;
+                if(quantity > 0)
+                {
+                    drawer.AddCoin(Coins.Nickle, quantity);
+                }
+                else
+                {
+                    drawer.RemoveCoin(Coins.Nickle, -quantity);
+                }
+                InvokePropertyChanged("Nickles");
+            }
+        }
+        //dimes
+        //quarters
+        //halfdollars
+        //Dollars
+        //ones
+        //twos
+        //fives
+        //tens
+        //twenties
+        //fiftties
+        //hundreds
+
+        void InvokePropertyChanged(string denomination)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(denomination));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TotalValue"));
+        }
+    }
+}
